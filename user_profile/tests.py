7fd2d4fdeffd9dc75fd7582e969707d55cf954f4
Vishuna <42171084+Vishuna@users.noreply.github.com>
2023-06-07 20:54:44 +0530

    Test Cases in Django Are Added
    Added Few Test Cases And what types of test cases can be occured mentioned in test.py file.

--- user_profile/tests.py
+++ user_profile/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
+from .models import *
+
 # Create your tests here.
+class StudentTestCase(TestCase):
+    def test_save_method(self):
+        student_Data=Student()
+        with self.assertRaises(Exception):
+            student_Data.save()
+
+
+# Test Cases used for:
+# 1. Field Validation
+# 2.Data Retrieval
+# 3.Form Validation
+# 4.View Functionality
+# 5.URL Routing
+# 6.Authentication and Authorization
+# 7.API Testing
+# 8.Error Handling
+# 9.File Uploads
+# 10.Custom Logic and Business Rule
