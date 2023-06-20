5f9eae74db4f160ebb593b42d70e89703a851ce7
Vishuna <42171084+Vishuna@users.noreply.github.com>
2023-06-20 18:07:58 +0530

    Download YouTube Video and show it to the next templates
    youtube link needs to put to download any video and store it in Media folder and form there we can all downloaded videos in django template

--- video/urls.py
+++ video/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns=[
  
     path('download/',views.download_video,name="download_video"),
+    path('videos/',views.video_view,name="view_videos"),
 ]
--- video/views.py
+++ video/views.py
@@ -3,6 +3,8 @@ from pytube import YouTube
 import os
 import requests
 from django.http import HttpResponse
+import glob
+from django.conf import settings
 
 
 def download_video(request):
@@ -16,10 +18,27 @@ def download_video(request):
                 stream=video.streams.get_lowest_resolution()
                 stream.download('media/video_uploads/')
 
-                return render(request,'video/video.html',{'msg':'video downloaded'})
+                return render(request,'video/download_video.html',{'msg':'video downloaded'})
             except:
-                 return render(request,'video/video.html',{'msg':'video not downloaded'})
-        return render(request,'video/video.html',{'msg':''})
+                 return render(request,'video/download_video.html',{'msg':'video not downloaded'})
+        return render(request,'video/download_video.html',{'msg':''})
     except:
-        return render(request,'video/video.html',{'msg':"Sorry something went wrong!"})
+        return render(request,'video/download_video.html',{'msg':"Sorry something went wrong!"})
     
+
+
+
+def video_view(request):
+    media_root=settings.MEDIA_ROOT
+    media_url=settings.MEDIA_URL
+    video_files=[]
+    media_path=os.path.join(media_root,'video_uploads')
+    for root,dirs,files in os.walk(media_path):
+                for file in files:
+                    file_path=os.path.join(root,file)
+                    relative_path=file_path.replace(media_root,'')
+                    print('mediaaaaaaaaa',relative_path)
+                    video_files.append(media_url+relative_path)
+    print(video_files)
+    context={'video_files':video_files}
+    return render(request,'video/view_video.html',context)
